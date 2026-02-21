97994c8ed3bc098b25b8c23e926fced9837620f2
sandeepkumar9760 <sandeeproofingsarvice@gmail.com>
2026-02-21 14:31:53 +0530

    initial commit

--- Block_stalls/Block_stalls/settings.py
+++ Block_stalls/Block_stalls/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
     "default": {
         'ENGINE': "django.db.backends.postgresql",
         'NAME': 'blockbites_db',
-        'USER':'POSTGRES',
+        'USER':'postgres',
         'PASSWORD':'123',
         'HOST':'localhost',
         'PORT':'5432',
