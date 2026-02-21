4bdb530082bae3b0d38e6d05871993e24510abac
sandeepkumar9760 <sandeeproofingsarvice@gmail.com>
2026-02-21 14:18:06 +0530

    Update admin.py

--- Block_stalls/backend_stalls/admin.py
+++ Block_stalls/backend_stalls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Block, Stall, MenuItem, TimeSlot, Order, OrderItem
+
+admin.site.register(Block)
+admin.site.register(Stall)
+admin.site.register(MenuItem)
+admin.site.register(TimeSlot)
+admin.site.register(Order)
+admin.site.register(OrderItem)
