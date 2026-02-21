a53ac65dc60328c98a1eb40cdc613e9d69062bd3
sandeepkumar9760 <sandeeproofingsarvice@gmail.com>
2026-02-21 20:21:32 +0530

    initial commit

--- Block_stalls/backend_stalls/urls.py
+++ Block_stalls/backend_stalls/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path , include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('blocks/', views.blocks, name='blocks'),
+    path('stalls/<int:block_id>/', views.stalls, name='stalls'),
 ]
--- Block_stalls/backend_stalls/views.py
+++ Block_stalls/backend_stalls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Block
+
+def blocks(request):
+    blocks = Block.objects.filter(is_active=True)
+    return render(request, 'block.html', {'blocks': blocks})
