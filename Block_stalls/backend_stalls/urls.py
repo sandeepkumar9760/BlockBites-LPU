d2ea462afd1055f2d5d42b26c02732df35c1e5fb
sandeepkumar9760 <sandeeproofingsarvice@gmail.com>
2026-02-22 22:23:39 +0530

    Update urls.py

--- Block_stalls/backend_stalls/urls.py
+++ Block_stalls/backend_stalls/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('update-cart/<int:item_id>/<str:action>/', views.update_cart, name='update_cart'),
 ]
