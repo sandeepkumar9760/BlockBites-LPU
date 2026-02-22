0ed988c3a777f4724b68c286dcd3425da7e0981a
sandeepkumar9760 <sandeeproofingsarvice@gmail.com>
2026-02-22 22:40:27 +0530

    initial commit

--- Block_stalls/Block_stalls/settings.py
+++ Block_stalls/Block_stalls/settings.py
@@ -57,12 +57,14 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [BASE_DIR / 'backend_stalls'/'templates'],
         "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
+       'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'backend_stalls.context_processors.cart_count',
+                ],
         },
     },
 ]
